be2d1b9158d940d4f6d37bfaf2fac1e9c5045968
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2019-11-28 10:14:14 -0500

    created some functions to send and receive data and process requests to the server

--- Server.py
+++ Server.py
@@ -3,6 +3,28 @@ import socket
 import dbHelper
 conn = dbHelper.Connection("127.0.0.1", "root", "", "PrimoEmail", False)
 
+# processes the request based on what the request type is
+def process_request(requestType, payload):
+   # if the client sends an email payload
+   if requestType == "emails":
+      insert_email(payload)
+   #if the client sends a message payload
+   elif requestType == "messages":
+      insert_message(payload)
+   # if a client wants to get data
+   elif requestType == "getData":
+      send_data_to_client(payload)
+
+# insert email into db
+def insert_email(payload):
+   conn.insert("emails", "message", [payload])
+# insert message into db
+def insert_message(payload):
+   conn.insert("messages", "message", [payload])
+# return data back to client
+def send_data_to_client():
+   pass
+
 # create a socket object
 serversocket = socket.socket(
 	        socket.AF_INET, socket.SOCK_STREAM) 
@@ -21,16 +43,10 @@ serversocket.listen(20)
 while True:
    # establish a connection
    clientsocket,addr = serversocket.accept()      
-
-   print("Got a connection from %s" % str(addr))
    header = clientsocket.recv(23).decode('ascii')
-   
-   messageType, size = header.split('|')
-   messageType = messageType.replace(' ', '')
-   print("type=", messageType, "|")
-   
-   if messageType != "blank":
-      size = int(size)
-      message = clientsocket.recv(size)
-      conn.insert(messageType, "message", [message])
+   requestType, size = header.split('|')
+   requestType = requestType.replace(' ', '')
+   size = int(size)
+   message = clientsocket.recv(size)
+   process_request(requestType, message)
    clientsocket.close()
