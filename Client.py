82bb2127811a07ae3107974b22dae263ee26810c
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2019-11-27 21:05:53 -0500

    split send_message up into send_message and send_email

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@
 import socket
 
 
-def send_message(type, message_text):
+def send_email(message):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     host = socket.gethostname()                           
 
@@ -11,13 +11,22 @@ def send_message(type, message_text):
 
     # connection to hostname on the port.
     s.connect((host, port))     
-    len_of_message = str(len(message_text))
-    if type != "emails" and type != "messages":
-        type = "blank"
-    elif type == "emails":
-        type += "  "
-    info = type + '|' + len_of_message.zfill(14)        
-    s.sendall(info.encode('ascii'))    
-    msg = message_text                           
-    s.sendall(msg.encode())
+    len_of_message = str(len(message))
+    info = "emails  " + '|' + len_of_message.zfill(14)        
+    s.sendall(info.encode('ascii'))                             
+    s.sendall(message.encode())
+    s.close()
+
+def send_message(message):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    host = socket.gethostname()                           
+
+    port = 9999
+
+    # connection to hostname on the port.
+    s.connect((host, port))     
+    len_of_message = str(len(message))
+    info = "messages" + '|' + len_of_message.zfill(14)        
+    s.sendall(info.encode('ascii'))                         
+    s.sendall(message.encode())
     s.close()
