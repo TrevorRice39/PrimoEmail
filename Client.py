866c21a5a43f2acd409f8cf12ab34fc3bfa8afee
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2019-11-29 11:11:40 -0500

    added create_header and new_socket functions in Client.py to shorten code

--- Client.py
+++ Client.py
@@ -3,56 +3,58 @@
 import socket
 import pickle
 
-def send_email(email):
+
+host = socket.gethostname()                           
+port = 9999
+header_max_length = 8 # length of longest header
+message_max_size = 14 # 10^14 bytes possible
+
+# creates a meta data header to request/send info from/to server
+def create_header(request_type, info):
+    len_info = str(len(info))
+    header = request_type.ljust(header_max_length) + '|' + len_info.zfill(message_max_size)
+    return header
+
+def new_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    host = socket.gethostname()                           
+    s.connect((host, port))  
+    return s
 
-    port = 9999
-
+def send_email(email):
+    s = new_socket(host, port)
     message = email.body
     pickled_email = pickle.dumps(email)
-    len_email = str(len(pickled_email))
-    # connection to hostname on the port.
-    s.connect((host, port))     
-    info = "emails  " + '|' + len_email.zfill(14)        
-    s.sendall(info.encode('ascii'))                             
+    header = create_header("emails", pickled_email)
+    s.sendall(header.encode('ascii'))                             
     s.sendall(pickled_email)
     s.close()
 
 def send_message(message):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    host = socket.gethostname()                           
-
-    port = 9999
+    s = new_socket(host, port)
     pickled_message = pickle.dumps(message)
-    len_message = str(len(pickled_message))
-    # connection to hostname on the port.
-    s.connect((host, port))     
-    info = "messages" + '|' + len_message.zfill(14)        
-    s.sendall(info.encode('ascii'))                         
+    header = create_header("messages", pickled_message)
+    s.sendall(header.encode('ascii'))                         
     s.sendall(pickled_message)
     s.close()
 
 def request_emails(bySender, address):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    host = socket.gethostname()                           
-
-    port = 9999
+    s = new_socket(host, port) 
     request = [bySender, address]
     pickled_request = pickle.dumps(request)
-    len_request = str(len(pickled_request))
-    # connection to hostname on the port.
-    s.connect((host, port))     
-    info = "getEmail" + '|' + len_request.zfill(14)        
-    s.sendall(info.encode('ascii'))                         
+    header = create_header("getEmail", pickled_request)    
+    s.sendall(header.encode('ascii'))                         
     s.sendall(pickled_request)
-    received_data = s.recv(20000)
+    received_data = b""
+    data = s.recv(5000)
+    while len(data) != 0:
+        received_data += data
+        data = s.recv(5000)
+    print(received_data)
     emails = pickle.loads(received_data)
     print(emails)
     for email in emails:
         print(email.sender, email.to, email.subject, email.body, email.time_sent)
     s.close()
-    pass
 
 def request_messages(chatroomId, index):
     pass
